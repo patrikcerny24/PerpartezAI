a7b052f782c87550d8e8df29482c4d501684c325
Mikond1 <mikond6@gmail.com>
2025-06-20 12:59:54 +0200

    12_errors_left

--- demo/src/main/java/com/examplecom/example/demo/UserController.java
+++ demo/src/main/java/com/examplecom/example/demo/UserController.java
@@ -6,7 +6,8 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
-public class UserController {
+
+public class UserController(){
     @Autowired
     private UserService userService;
 
