fab0410797af8eb87060ad4e58c3cb2c10dce260
patrikcerny24 <pata.cerny@centrum.cz>
2025-06-20 13:53:44 +0300

    idk

--- demo/src/main/java/com/examplecom/example/demo/UsersContoller.java
+++ demo/src/main/java/com/examplecom/example/demo/UsersContoller.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 @RestController
 @RequestMapping("/user")
-public class UserController() {
+public class UserController {
     @Autowired
     private UserService userService;
 
--- demo/src/main/java/com/examplecom/example/demo/UsersInterface.java
+++ demo/src/main/java/com/examplecom/example/demo/UsersInterface.java
@@ -1,6 +1,5 @@
 package com.example.demo;
 
-import com.example.demo.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
