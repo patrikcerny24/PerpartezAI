f9eaeb7bd63a00db4a8c25c223bd85f17ce29b30
Jakub Líska <liskajakub612@outlook.com>
2025-06-20 14:26:01 +0300

    Bootstrap

--- perpartezaifront/src/index.js
+++ perpartezaifront/src/index.js
@@ -3,5 +3,5 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Layout from"./pages/layout";
 import Home from "./pages/home";
 import About from "./pages/aboutus";
-
+import Bootstrap from 'bootstrap/dist/css/bootstrap.min.css';
 
