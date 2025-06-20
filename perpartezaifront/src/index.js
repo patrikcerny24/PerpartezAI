d7655a7ed807646582c06edf1c85b56a5d87db2d
Filip Novak <nfilip989@gmail.com>
2025-06-20 13:39:25 +0300

    Další lehčí úprava struktury

--- perpartezaifront/src/index.js
+++ perpartezaifront/src/index.js
@@ -1,1 +1,8 @@
-import 
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Layout from"./pages/layout";
+import Home from "./pages/home";
+import About from "./pages/aboutus";
+
+
