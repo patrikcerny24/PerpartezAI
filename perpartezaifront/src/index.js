f52fe60f2d34a0c3eb7895ba51261b987d938e3d
Filip Novak <nfilip989@gmail.com>
2025-06-20 14:03:22 +0300

    základní routování

--- perpartezaifront/src/index.js
+++ perpartezaifront/src/index.js
@@ -1,8 +1,24 @@
-import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import Layout from"./pages/layout";
-import Home from "./pages/home";
-import About from "./pages/aboutus";
+import Layout from"./pages/Layout";
+import Home from "./pages/Home";
+import About from "./pages/Aboutus";
+import NoPage from "./pages/Nopage";
 
 
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="aboutus" element={<About />} />
+          <Route path="*" element={<NoPage />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<App />);
