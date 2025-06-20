3ccd5e63ad6666700370debe7bcacd26d60836bd
Filip Novak <nfilip989@gmail.com>
2025-06-20 12:32:54 +0300

    App.js

---
+++ perpartezaifront/src/App.js
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to the PerpartezAI App</h1>
+        <p>This is a simple React application.</p>
+      </header>
+    </div>
+  );
+} 
+export default App; hdjd
