f8b1d3421ae35395323dfd1673e19eab11a4e921
Mikond1 <mikond6@gmail.com>
2025-06-20 13:06:40 +0200

    psuh_up_the_fucking_shit_that_doesnt_fucking)_work

--- demo/src/main/java/com/examplecom/example/demo/UserController.java
+++ demo/src/main/java/com/examplecom/example/demo/UserController.java
@@ -7,7 +7,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/user")
 
-public class UserController(){
+public class UserController{
     @Autowired
     private UserService userService;
 
