3191db008da169256f5c6701e2655f67aa7a1e22
patrikcerny24 <pata.cerny@centrum.cz>
2025-06-25 13:39:55 +0300

    pokus o vyhozeni duplikatu

--- demo/src/main/java/com/examplecom/example/demo/UserRepository.java
+++ demo/src/main/java/com/examplecom/example/demo/UserRepository.java
@@ -2,11 +2,13 @@ package com.examplecom.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-import org.springframework.boot.CommandLineRunner;
+/*import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Configuration;*/
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
 }
 
