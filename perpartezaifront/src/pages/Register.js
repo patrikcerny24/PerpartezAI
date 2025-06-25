e763f719b558cb2f8d57194d71828017bd3b258d
Filip Novak <nfilip989@gmail.com>
2025-06-25 14:01:15 +0300

    Dodělání registru

--- perpartezaifront/src/pages/Register.js
+++ perpartezaifront/src/pages/Register.js
@@ -1,19 +1,83 @@
-import React from 'react';
+import React,{useState} from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 const Register = () => {
+    const [formData, setFormData] = useState({
+        name:"",
+        surname:"",
+        username:"",
+        email:"",
+        password:"",
+        birthdate:"",
+    });
+    const handleChange = (e) => {
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value
+    }));
+  };
+ const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('http://localhost:8080/h2-console/login.do?jsessionid=cc265933cf2580d4540d1585c6968fc8', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(formData)
+      });
+
+      if (!response.ok) {
+        throw new Error('Failed to register');
+      }
+
+      const result = await response.json();
+      alert('Registration successful!');
+      console.log(result);
+    } catch (error) {
+      console.error('Error:', error.message);
+      alert('Registration failed.');
+    }
+  };
+
   return (
     <div className="container">
         <div className="row">
             <div className="col-md-6 offset-md-3">
                 <h2 className="text-center">Registrovat se</h2>
-                <form>
+                <form onSubmit={handleSubmit}>
                     <div className="form-group">
                         <label htmlFor="name">Jméno:</label>
                         <input
                             type="text" 
                             className="form-control"
                             id="name"
+                            value={formData.name}
+                            onChange={handleChange}
                             placeholder="Zadejte své jméno"
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="surename">Příjmení:</label>
+                        <input
+                            type="text" 
+                            className="form-control"
+                            id="surename"
+                            value={formData.surname}
+                            onChange={handleChange}
+                            placeholder="Zadejte své příjmení"
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="username">Uživatelské jméno:</label>
+                        <input
+                            type="text" 
+                            className="form-control"
+                            id="username"
+                            value={formData.username}
+                            onChange={handleChange}
+                            placeholder="Zadejte své uživatelské jméno"
                             required
                         />
                     </div>
@@ -23,7 +87,20 @@ const Register = () => {
                             type="email"
                             className="form-control"
                             id="email"
+                            value={formData.email}
+                            onChange={handleChange}
                             placeholder="Zadejte svůj email"
+                            required
+                        />
+                    </div>
+                    <div className="form-group">
+                        <label htmlFor="birthdate">Datum narození:</label>
+                        <input
+                            type="date" 
+                            className="form-control"
+                            id="birthdate"
+                            value={formData.birthdate}
+                            onChange={handleChange}
                             required
                         />
                     </div>
@@ -33,6 +110,8 @@ const Register = () => {
                             type="password"
                             className="form-control"
                             id="password"
+                            value={formData.password}
+                            onChange={handleChange}
                             placeholder="Zadejte své heslo"
                             required
                         />
--- perpartezaifront/src/pages/home.js
+++ perpartezaifront/src/pages/home.js
@@ -1,18 +1,19 @@
 import React from 'react';
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import './Home.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 const Home = () => {
     return (
         <>
-        <div className="container-fluid bg-light text-">     
-        <p>Home</p>
-        </div>
-        <footer className="conatiner-fluid">
-            <div>
-                <p>Kontakt: <a href="mailto:"></a></p>
-            </div>
-            <div>
-                <p>&copy; 2025 Perpartezai, Všechna práva vyhrazena.</p>
-            </div>
-        </footer>
+        <Container fluid className="bg-light">
+            <Row>
+                <Col>
+                </Col>
+            </Row>
+        </Container>    
+       
         </>
     );
 }
