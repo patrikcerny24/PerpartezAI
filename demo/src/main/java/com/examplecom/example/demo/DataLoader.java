bcced95edf3cce1850128a39e8c83122d4f4d395
patrikcerny24 <pata.cerny@centrum.cz>
2025-06-25 12:37:09 +0300

    Database working les goooo

---
+++ demo/src/main/java/com/examplecom/example/demo/DataLoader.java
@@ -0,0 +1,17 @@
+package com.examplecom.example.demo;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class DataLoader {
+
+    @Bean
+    CommandLineRunner initDatabase(UserRepository repo) {
+        return args -> {
+            repo.save(new User("Alice", "Smith", "alice", "pass1", "alice@example.com", 1986));
+            repo.save(new User("Bob",   "Jones", "bobby", "pass2", "bob@example.com",   1979));
+        };
+    }
+}
--- demo/src/main/java/com/examplecom/example/demo/UserRepository.java
+++ demo/src/main/java/com/examplecom/example/demo/UserRepository.java
@@ -2,7 +2,11 @@ package com.examplecom.example.demo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 }
+
