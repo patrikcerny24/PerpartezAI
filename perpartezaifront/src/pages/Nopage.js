2da5d07fc2adaa1065652a45a0c0122423881057
Filip Novak <nfilip989@gmail.com>
2025-06-25 18:35:50 +0300

    lehká úprava nopage

--- perpartezaifront/src/pages/Nopage.js
+++ perpartezaifront/src/pages/Nopage.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import './Nopage.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 const Nopage = () => {
   return (
-    <div className="container"> 
-    <p>Error 404 stránku se nepodařilo najít</p>
+    <div className="container justify-content-center text-center bg-warning "> 
+    <h1>Error 404 stránku se nepodařilo najít !</h1>
+    <button><a href="/">Zpět na domovskou stránku</a></button>
     </div>
   );
 }
